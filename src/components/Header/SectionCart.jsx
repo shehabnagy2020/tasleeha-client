f3bd030fd2e3bef94f77fc2793ac74f280b01d6d
shehabnagy1998 <shehab.nagy2020@gmail.com>
2021-10-03 15:13:00 +0200

    fixed cart in header

--- src/components/Header/SectionCart.jsx
+++ src/components/Header/SectionCart.jsx
@@ -7,16 +7,21 @@ const SectionCart = () => {
   return (
     <a href="/cart" className="flex items-center flex-shrink-0">
       <div className="hidden md:flex mr-1">
-        <span className="text-sm text-gray-400 font-bold capitalize">
-          ${cartItems.total}
-        </span>
+        {cartItems?.items?.length >= 1 && (
+          <span className="text-sm text-gray-400 font-bold capitalize">
+            ${cartItems.total}
+          </span>
+        )}
       </div>
       <div className="relative">
-        <div className="absolute" style={{ bottom: "60%", left: "60%" }}>
-          <span className="bg-red-400 px-2 py-1 rounded-full text-white text-sm">
-            {cartItems?.items?.length}
-          </span>
-        </div>
+        {cartItems?.items?.length >= 1 && (
+          <div className="absolute" style={{ bottom: "60%", left: "60%" }}>
+            <span className="bg-red-400 px-2 py-1 rounded-full text-white text-sm">
+              {cartItems?.items?.length}
+            </span>
+          </div>
+        )}
+
         <div className="flex justify-center items-center text-gray-400">
           <span className="material-icons">shopping_cart</span>
         </div>
---
+++ src/components/PageUser/SectionOrders.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SectionOrders = () => {
+  return <div></div>;
+};
+
+export default SectionOrders;
