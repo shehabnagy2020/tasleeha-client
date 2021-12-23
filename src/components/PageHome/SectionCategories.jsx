70e50978a94543f9bdf4f0e73e1d5239345a0e39
shehabnagy1998 <shehab.nagy2020@gmail.com>
2021-12-23 12:30:06 -0800

    fixed categories in home

--- src/components/PageHome/SectionCategories.jsx
+++ src/components/PageHome/SectionCategories.jsx
@@ -56,7 +56,7 @@ const SectionCategories = () => {
           جميع الفئات
         </h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          {categoryItems?.map((item) => (
+          {categoryItems?.all?.map((item) => (
             <a
               href={`/category/${item.id}`}
               key={item.id}
